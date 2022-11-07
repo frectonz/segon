45806e7589d46c3257b8d2c297bb59d55946ba27
frectonz <fraol0912@gmail.com>
2022-11-07 19:43:50 +0300

    fix typo

--- src/adapters/notifier.rs
+++ src/adapters/notifier.rs
@@ -1,27 +1,31 @@
 use crate::ports::GameStartNotifier;
 use async_trait::async_trait;
 use std::sync::Arc;
-use tokio::sync::{mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender}, Mutex};
+use tokio::sync::{
+    mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
+    Mutex,
+};
 
 #[derive(Clone)]
 pub struct Notifier {
-    reciever: Arc<Mutex<UnboundedReceiver<()>>>,
-    sender: UnboundedSender<()>
+    receiver: Arc<Mutex<UnboundedReceiver<()>>>,
+    sender: UnboundedSender<()>,
 }
 
 impl Notifier {
     pub fn new() -> Self {
         let (sender, receiver) = unbounded_channel::<()>();
-        Self { 
-            reciever: Arc::new(Mutex::new(receiver)), 
-            sender }
+        Self {
+            sender,
+            receiver: Arc::new(Mutex::new(receiver)),
+        }
     }
 }
 
 #[async_trait]
 impl GameStartNotifier for Notifier {
     async fn wait_for_signal(&self) -> Option<()> {
-        let mut rx = self.reciever.lock().await;
+        let mut rx = self.receiver.lock().await;
         rx.recv().await
     }
 
