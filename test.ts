c84cb70da5661a1024de136c216eeb32e52ee315
frectonz <fraol0912@gmail.com>
2022-11-15 11:56:13 +0300

    multi user test

--- test.ts
+++ test.ts
@@ -1,28 +1,7 @@
-const user = { username: "test", password: "test" };
-
-const register = await fetch("http://localhost:3030/register", {
-  method: "POST",
-  body: JSON.stringify(user),
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-const registerResponse: {
-  status: string;
-  token: string;
-} = await register.json();
-
-console.log("RESPONSE", registerResponse);
-
-// websocket client
-const ws = new WebSocket(`ws://localhost:3030/game/${registerResponse.token}`);
-
-ws.onopen = () => {
-  ws.send(JSON.stringify({ type: "Answer", answer_idx: "Three" }));
-  console.log("connected");
-}
-
+type User = {
+  username: string;
+  password: string;
+};
 type Message = {
   type: "TimeTillGame",
   time: number
@@ -43,30 +22,61 @@ type Message = {
   type: "GameStart"
 };
 
-ws.onmessage = (event) => {
-  const msg: Message = JSON.parse(event.data)
 
-  if (msg.type === "TimeTillGame") {
-    console.log("Time till game", msg.time)
-  } else if (msg.type === "Question") {
-    console.log(" ", msg.question);
-    for (const option of msg.options) {
-      console.log("  ", option);
-    }
-    ws.send(JSON.stringify({ type: "Answer", answer_idx: "One" }));
-  } else if (msg.type === "Answer") {
-    console.log(" Answer status", msg.status);
-    console.log(" Answer is option", msg.answer_idx);
-  } else if (msg.type === "NoGame") {
-    console.log("No game");
-  } else if (msg.type === "GameEnd") {
-    console.log("Game ended with score", msg.score);
-  } else if (msg.type === "GameStart") {
-    console.log("Game started");
+async function test(user: User) {
+  const register = await fetch("http://localhost:3030/register", {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const registerResponse: {
+    status: string;
+    token: string;
+  } = await register.json();
+
+  const label = user.username + " : "
+  console.log(label, registerResponse);
+
+  // websocket client
+  const ws = new WebSocket(`ws://localhost:3030/game/${registerResponse.token}`);
+
+  ws.onopen = () => {
+    ws.send(JSON.stringify({ type: "Answer", answer_idx: "Three" }));
+    console.log(label, "connected");
   }
 
-};
+  ws.onmessage = (event) => {
+    const msg: Message = JSON.parse(event.data)
 
-ws.onclose = () => {
-  console.log("disconnected");
+    if (msg.type === "TimeTillGame") {
+      console.log(label, "Time till game", msg.time)
+    } else if (msg.type === "Question") {
+      console.log(label, msg.question);
+      for (const option of msg.options) {
+        console.log(label, option);
+      }
+      ws.send(JSON.stringify({ type: "Answer", answer_idx: "One" }));
+    } else if (msg.type === "Answer") {
+      console.log(label, "Answer status", msg.status);
+      console.log(label, "Answer is option", msg.answer_idx);
+    } else if (msg.type === "NoGame") {
+      console.log(label, "No game");
+    } else if (msg.type === "GameEnd") {
+      console.log(label, "Game ended with score", msg.score);
+    } else if (msg.type === "GameStart") {
+      console.log(label, "Game started");
+    }
+
+  };
+
+  ws.onclose = () => {
+    console.log(label, "disconnected");
+  }
+}
+
+for (let i = 0; i < 10; i++) {
+  test({ username: `user-${i}`, password: "password" });
 }
